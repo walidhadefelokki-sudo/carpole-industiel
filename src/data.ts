e2263ee74968cb23d92816efb51b54809c4e866a
SalohKhb <salohsoh2006@gmail.com>
2026-07-13 13:30:54 +0100

    update

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ import imageFrigo from './assets/images/image2.jpeg';
 import imageConstantinePorteur from './assets/images/image1.jpeg';
 import image1 from './assets/images/image1.jpeg';
 import image3 from './assets/images/image3.jpeg';
-import image4 from './assets/images/image4.jpeg';
+import image4 from './assets/images/truck_frigo_installation_1781872814520.jpg';
 import image5 from './assets/images/image5.jpeg';
 import image6 from './assets/images/image6.jpeg';
 import image7 from './assets/images/image7.jpeg';
